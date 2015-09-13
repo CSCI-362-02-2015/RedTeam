d012a9732665d7112fe6330440c52037aabffb9c
Kyle Brooks <brookska@g.cofc.edu>
2015-09-13 14:36:17 -0400

    Added ChapterOne.pdf

--- myList.sh
+++ myList.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 echo "<h1 style="color:red"> Contents of $PWD:</h1>" >Contents.html
+echo "<HTML>" >> Contents.html
+echo "<body>" >> Contents.html
 ls -1>> Contents.html
+echo "</body>" >> Contents.html
+echo "</HTML>"
 xdg-open Contents.html
 
