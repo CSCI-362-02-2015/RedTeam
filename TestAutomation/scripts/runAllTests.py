72df9e3b70fef2e7d1ba165056aeca9acac59d99
Kyle Brooks <brookska@g.cofc.edu>
2015-11-08 12:22:25 -0500

    Modified runAllTests, removal of some unnecessary files

--- TestAutomation/scripts/runAllTests.py
+++ TestAutomation/scripts/runAllTests.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+
 # runAllTests.py
 
 import os, sys
@@ -5,80 +7,119 @@ import glob
 
 #Change working directory to testCasesExecutables
 
-#testCasePath = os.testCasePath.expanduser("~/RedTeam/TestAutomation/testCases/")
-path = os.getcwd() 
-testCasePath = os.path.abspath(os.path.join(path, os.pardir)) + "/testCases"
-reportPath = os.path.abspath(os.path.join(path, os.pardir)) + "/reports"
 
 
-os.chdir(testCasePath) 
-listing = os.listdir(testCasePath)
-listing.sort()
-import_base = "from mercurial import "
+def main():
+  #testCasePath = os.testCasePath.expanduser("~/RedTeam/TestAutomation/testCases/")
+  path = os.getcwd() 
+  testCasePath = os.path.abspath(os.path.join(path, os.pardir, "testCases"))
+  reportPath = os.path.abspath(os.path.join(path, os.pardir, "reports/report.html"))
+  os.chdir(testCasePath) 
+  listing = os.listdir(testCasePath)
+  listing.sort()
+  import_base = "from mercurial import "
 
-#Counts the test cases in the testCases folder
-count = len(listing)
+  #Counts the test cases in the testCases folder
+  count = len(listing)
+
+  writeOpeningInfo(reportPath)
+  report = open(reportPath, 'a+')
 
 
-report = open(reportPath + "/report.html", 'w+')
-report.write("<html><body>")
 
-#If statement to check if the folder has test cases
-if count < 1:
-  report.write("No test cases found in test directory")
-  print "There are no test cases.  Be Better..."
-else:
-        passCount = 0 #number of passed tests
-        failCount = 0 #numer of failed tests
-        
-        for infile in listing:
-                #print "Current File: " + infile
-                try:
-                        with open(testCasePath + '/' + infile, 'r') as f:
-                                testList = f.read().splitlines()
-                        iden = testList[0]
-                        req = testList[1]
-                        component = testList[2]
-                        method = testList[3]
-                        inp = testList[4]
-                        outcome = testList[5]
+  #If statement to check if the folder has test cases
+  if count < 1:
+    report.write("No test cases found in test directory")
+    print "There are no test cases.  Be Better..."
+  else:
+          passCount = 0 #number of passed tests
+          failCount = 0 #numer of failed tests
+          
+          for infile in listing:
+                  #print "Current File: " + infile
+                  try:
+                          with open(testCasePath + '/' + infile, 'r') as f:
+                                  testList = f.read().splitlines()
+                          iden = testList[0]
+                          req = testList[1]
+                          component = testList[2]
+                          method = testList[3]
+                          inp = testList[4]
+                          outcome = testList[5]
 
-                        print "ID: ", iden
+                          print "ID: ", iden
 
-                        
+                          passFormat = '<tr bgcolor="#00FF00"><td>{}</td><td>{}</td><td>TODO</td><td>{}</td><td>{}</td></tr>'
+                          failFormat = '<tr bgcolor ="#FF0000"><td>{}</td><td>{}</td><td>Outcome</td><td>{}</td><td>{}</td></tr>'
 
-                        #Execute test file script
-                
-                        import_module = import_base + component
-                        exec( import_module )
-                        
-                        print "Requirements: ", req
-                        print "Component: ", component
-                        print "Method: ", method
-                        print "Input: ", inp
-                        print "Expected Outcome: ", outcome
-                        statement = 'print(' + component + "." + method + "(" + inp + ")" + ")"
-                        exec(statement)
-                        passCount += 1
-                        report.write("{}&nbsp{}&nbsp{}&nbsp{}<br>".format(iden, "test", outcome, "pass"))
-                        
-                except:
-                        failCount += 1
-                        e = sys.exc_info()[0]
-                        print "Unable to execute TestID[" + iden +"]"
-                        print "Please check formatting and inputs of test case"
-                        print(e)
-                        print 
-                        print "Continuing..."
-                f.close()
-                print('\n')
+                          #Execute test file script
+                  
+                          import_module = import_base + component
+                          exec( import_module )
+                          
+                          print "Requirements: ", req
+                          print "Component: ", component
+                          print "Method: ", method
+                          print "Input: ", inp
+                          print "Expected Outcome: ", outcome
+                          statement = 'print(' + component + "." + method + "(" + inp + ")" + ")"
+                          ##statement = 'print(' + component + "." + method + "(" + inp + ")" + "> testing.txt)"
 
-        report.write("</body></html>")
-        #Print Stats at the end        
-        print "Number of Tests: {}".format(count)
-        print "Passed: {}".format(passCount)
-        print "Failed: {}\n".format(failCount)
-        print "Done"
-        report.close()
+                          exec(statement)
 
-        
+                          passCount += 1
+                          passFormat = passFormat.format(iden, inp, outcome, "PASS")
+                          report.write(passFormat)
+                  except:
+                          failCount += 1
+                          failFormat = failFormat.format(iden, inp, outcome, "FAIL")
+                          report.write(failFormat)
+                          e = sys.exc_info()[0]
+                          print "Unable to execute TestID[" + iden + "]"
+                          print "Please check formatting and inputs of test case"
+                          print(e)
+                          print 
+                          print "Continuing..."
+                  
+                  print('\n')
+          f.close()
+          report.write("</table></body></html>")
+          report.close()
+          printSummary(count, passCount, failCount)
+
+def writeOpeningInfo(reportPath):
+  
+  report = open(reportPath, 'w+')
+  report.write("<!DOCTYPE html>")
+  report.write("<html>")
+  report.write("<body>")
+  report.write('<table border="1" style =""width:100%>')
+  report.write("<tr>")
+  report.write("<td><b>Test ID</b></td>")
+  report.write("<td><b>Input</b></td>")
+  report.write("<td><b>Output</b></td>")
+  report.write("<td><b>Expected</b></td>")
+  report.write("<td><b>Pass/Fail</b></td>")
+  report.close()
+
+
+def printSummary(count, passCount, failCount):
+  #Print Stats at the end        
+  print "Number of Tests: {}".format(count)
+  print "Passed: {}".format(passCount)
+  print "Failed: {}\n".format(failCount)
+  print "Done"
+
+if __name__ == "__main__":
+  main()
+
+
+
+
+
+
+
+
+
+
+  
