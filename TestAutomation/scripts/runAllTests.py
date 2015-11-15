3327c0888541a9f53f457c7ba5a48887132667c1
Kyle Brooks <brookska@g.cofc.edu>
2015-11-15 12:50:40 -0500

    Corrected testCase requirements

--- TestAutomation/scripts/runAllTests.py
+++ TestAutomation/scripts/runAllTests.py
@@ -1,16 +1,29 @@
+#!/usr/bin/python
+
 #CSCI 362 Group Project
 #RED TEAM
 #Kyle Brooks
 #Jesse Hunt
 #Chris Sigmund
 
-#!/usr/bin/python
+'''
+The purpose of this script is to test functions implemented in the
+OSS Mercurial. The script reads .txt files located in
+the folder RedTeam/TestAutomation/testCases and uses the information from
+them to compile and run a test case. The test case files contain the name
+of the class and method to be tested, as well as the input given. The value
+returned by the eval command is checked against an oracle value inside the
+test case text file.
 
-# runAllTests.py
+
+To write test cases, follow the instructions in the
+README.txt file, located in /RedTeam/
+'''
+
 
 import os, sys, glob, StringIO, contextlib, webbrowser
 
-
+# Global variables, to cut down on parameter passing
 path = os.getcwd() 
 testCasePath = os.path.abspath(os.path.join(path, os.pardir, "testCases"))
 reportPath = os.path.abspath(os.path.join(path, os.pardir, "reports/report.html"))
@@ -36,8 +49,14 @@ def main():
      print "There are no test cases.  Be Better..."
   else:
      runTests(report, count)
-     webbrowser.get('Firefox').open(reportPath)
+     openReport()
 
+  print("Done")
+
+
+#runTests is where the lion's share of the work is done
+#it reads in each test case file individually, compiles
+#and runs the code, then evaluates this against the oracle value
 def runTests(report, count):
 
         passCount = 0 #number of passed tests
@@ -53,11 +72,14 @@ def runTests(report, count):
                                 method = testList[3]
                                 inp = testList[4]
                                 outcome = testList[5]
+                                compAndMethod = component + "." + method + "()"
                                 
                                 print "ID: ", iden
-                                
-                                passFormat = '<tr bgcolor="#00FF00"><td>{}</td><td>{}</td><td>{}</td><td>{}</td><td>{}</td></tr>'
-                                failFormat = '<tr bgcolor ="#FF0000"><td>{}</td><td>{}</td><td>{}</td><td>{}</td><td>{}</td></tr>'
+
+
+                                ##Format: TestID, Component/Method, Input, Expected Output, Actual Output, Pass/Fail
+                                passFormat = '<tr bgcolor="#00FF00"><td>{}</td><td>{}</td><td>{}</td><td>{}</td><td>{}</td><td>{}</td></tr>'   
+                                failFormat = '<tr bgcolor ="#FF0000"><td>{}</td><td>{}</td><td>{}</td><td>{}</td><td>{}</td><td>{}</td></tr>'
 
 
                                 #Execute test file script
@@ -78,17 +100,17 @@ def runTests(report, count):
 
                                 if (str(result) == str(outcome)):
                                         passCount += 1
-                                        passFormat = passFormat.format(iden, inp, outcome, str(result), "PASS")
+                                        passFormat = passFormat.format(iden, compAndMethod, inp, outcome, str(result), "PASS")
                                         report.write(passFormat)
                                         print("PASS\n")
                                 else:
                                         failCount += 1
-                                        failFormat = failFormat.format(iden, inp, outcome, str(result), "FAIL")
+                                        failFormat = failFormat.format(iden, compAndMethod, inp, outcome, str(result), "FAIL")
                                         report.write(failFormat)
                                         print("FAIL\n")
                 except:
                         failCount += 1
-                        failFormat = failFormat.format(iden, inp, outcome, "ERROR", "FAIL")
+                        failFormat = failFormat.format(iden, compAndMethod, inp, outcome, "ERROR", "FAIL")
                         report.write(failFormat)
                         e = sys.exc_info()[0]
                         print "Unable to execute TestID[" + iden + "]"
@@ -106,10 +128,15 @@ def writeOpeningHTML():
   report = open(reportPath, 'w+')
   report.write("<!DOCTYPE html>")
   report.write("<html>")
+  report.write("<head>")
+  report.write("<style>")
+  report.write("table, th, td {border: 1px solid black; border-collapse: collapse;}")
+  report.write("</style></head>")
   report.write("<body>")
   report.write('<table border="1" style =""width:100%>')
   report.write("<tr>")
   report.write("<td><b>Test ID</b></td>")
+  report.write("<td><b>Component.method()</b></td>")
   report.write("<td><b>Input</b></td>")
   report.write("<td><b>Expected Output</b></td>")
   report.write("<td><b>Actual Output</b></td>")
@@ -131,11 +158,17 @@ def printSummary(count, passCount, failCount):
   print "Number of Tests: {}".format(count)
   print "Passed: {}".format(passCount)
   print "Failed: {}\n".format(failCount)
-  print "Done"
 
-
-
-
+#May not be the most elegant way of doing things
+#assumes user has Chrome installed
+#otherwise attempts to open Firefox
+def openReport():
+  try:
+    chrome_path = 'open -a /Applications/Google\ Chrome.app %s'
+    webbrowser.get(chrome_path).open(reportPath)
+  except:
+    webbrowser.get('Firefox').open(reportPath)
+    
 
 
 if __name__ == "__main__":
